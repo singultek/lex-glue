d2d3f5b63c315037b74178719ce58de615cd518a
singultek <singultek@gmail.com>
2023-12-14 21:07:49 +0100

    Preparation of scripts using Lora Adapters

--- scripts/eurlex/run_eurlex_llama2_7b.sh
+++ scripts/eurlex/run_eurlex_llama2_7b.sh
@@ -1,10 +1,14 @@
-GPU_NUMBER=0
-MODEL_NAME='NousResearch/Llama-2-7b-hf'
+GPU_NUMBER=3
+MODEL_NAME='meta-llama/Llama-2-7b-hf'
 LOWER_CASE='True'
-BATCH_SIZE=32
+BATCH_SIZE=1
 ACCUMULATION_STEPS=1
 TASK='eurlex'
+TRAIN_EPOCH=3
+LORA='True'
+MAX_SEQ_LENGTH=512
+USE_AUTH_TOKEN=True
 
-CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ./experiments/eurlex.py --model_name_or_path ${MODEL_NAME} --do_lower_case ${LOWER_CASE} --max_seq_length 64 --output_dir logs/${TASK}/${MODEL_NAME}/seed_1 --do_train --do_eval --do_pred --overwrite_output_dir --load_best_model_at_end --metric_for_best_model micro-f1 --greater_is_better True --evaluation_strategy epoch --save_strategy epoch --save_total_limit 5 --num_train_epochs 2 --learning_rate 3e-5 --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --seed 1 --fp16 --fp16_full_eval --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
+CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python ./experiments/eurlex.py --model_name_or_path ${MODEL_NAME} --lora ${LORA} --use_auth_token ${USE_AUTH_TOKEN} --max_seq_length ${MAX_SEQ_LENGTH} --do_lower_case ${LOWER_CASE} --task ${TASK} --output_dir logs/${TASK}/${MODEL_NAME}/seed_1 --do_train --do_eval --do_pred --overwrite_output_dir --load_best_model_at_end --metric_for_best_model micro-f1 --greater_is_better True --evaluation_strategy epoch --save_strategy epoch --save_total_limit 5 --num_train_epochs ${TRAIN_EPOCH} --learning_rate 3e-5 --per_device_train_batch_size ${BATCH_SIZE} --per_device_eval_batch_size ${BATCH_SIZE} --seed 1 --fp16 True --fp16_full_eval True --gradient_accumulation_steps ${ACCUMULATION_STEPS} --eval_accumulation_steps ${ACCUMULATION_STEPS}
 
 python ./statistics/compute_avg_scores.py --dataset ${TASK}
